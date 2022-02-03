cfe573021ad68ed9973a55c0898d554e593dc874
Chase Young <chasekyoung@gmail.com>
2022-02-03 13:45:26 -0500

    changed button tags to anchor tags

--- assets/script.js
+++ assets/script.js
@@ -8,4 +8,17 @@ buttons.addEventListener("click", getGenre)
 function getGenre(event) {
     console.log(event.target.getAttribute("data-value"))
     var genreInput = event.target.getAttribute("data-value")
+    fetch(`https://data-imdb1.p.rapidapi.com/movie/byGen/${genreInput}/?page_size=50`, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "data-imdb1.p.rapidapi.com",
+		"x-rapidapi-key": "279b04dcfemshc0a23d26e174c8ep132e8cjsn6c46292bbe34"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
 }
