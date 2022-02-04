b162989c525a4a25d5fd9fffab6f68e635e7d3c2
Ramil Rakhamimov <rakhamimovramil@gmail.com>
2022-02-04 16:55:48 -0500

    Restructuring

--- assets/script.js
+++ assets/script.js
@@ -2,18 +2,22 @@ var buttons = document.querySelector("#container");
 var genre = document.querySelector(".btn");
 var value = genre.getAttribute("data-value");
 var movieRating = document.querySelector(".ratedBtn");
-var ratingValue = movieRating.getAttribute("data-value");
+//var ratingValue = movieRating.getAttribute("data-value");
 var movieArray = [];
-var userInputRated = ''
-var userInputRuntime = ''
-var userInputReviews = ''
-var movieTitle = ''
+var finalMovieArray = [];
+// var userInputRated = ''
+// var userInputRuntime = ''
+// var userInputReviews = ''
+// var movieTitle = ''
+var ratingArr = ['G', 'PG', "PG-13", "R", "NC-17"];
 
 
 buttons.addEventListener("click", getGenre)
     
 
 function getGenre(event) {
+	document.getElementById('sub-container').classList.remove('hide');
+
     var genreInput = event.target.getAttribute("data-value")
     fetch(`https://data-imdb1.p.rapidapi.com/movie/byGen/${genreInput}/?page_size=5`, {
 	"method": "GET",
@@ -27,6 +31,16 @@ function getGenre(event) {
 })
 .then(data => {
     var resultsArray = data.results
+	ratingArr.forEach(function(rating){
+		var btn = document.createElement('button');
+		btn.setAttribute('class', `${rating} item ratedBtn`);
+		btn.setAttribute('value', rating)
+
+		btn.onclick = filterChoice;
+
+		document.getElementById('sub-container').append(btn)
+	})
+
 	for (let i = 0; i < resultsArray.length; i++) {
 		var imdbID = data.results[i].imdb_id
 		passAlongData(imdbID)
@@ -46,13 +60,29 @@ function passAlongData(iD) {
 		return response.json();
 	})
 	.then(data => {
-		console.log(data)
-		//movieArray.push({title: data.Title, rated: data.Rated, runtime: data.Runtime, reviews: data.imdbRating});
+		//console.log(data)
+		movieArray.push({title: data.Title, rated: data.Rated, runtime: data.Runtime, reviews: data.imdbRating});
 		// console.log(movieArray)
-		if (data.Rated == 'The Suicide Squad') {
-		   localStorage.setItem('title',)	
-		}
+	
+		
+		// if (data.Rated == 'The Suicide Squad') {
+		//    localStorage.setItem('title',)	
+		// }
 	})
+}
+
+function filterChoice(){
+	console.log(this.value)
+
+	for (let index = 0; index < movieArray.length; index++) {
+		
+		if (this.value === movieArray[index].rated) {
+			finalMovieArray.push(movieArray[index])
+			
+		}
+		
+	}
+	console.log(finalMovieArray)
 }
 console.log(movieArray)
 // for (let index = 0; index < movieArray.length; index++) {
